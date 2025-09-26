1a9ee1816347c0b3e3ae12784b0c82ac3c1912b6
Juliana Braga <jujbraga@gmail.com>
2025-09-26 11:51:49 +0200

    add draw random numbers function

--- scripts.js
+++ scripts.js
@@ -4,3 +4,36 @@ const inputNumber = document.getElementById("numbers");
 const fromNumber = document.getElementById("from");
 const toNumber = document.getElementById("to");
 const uniqueNumber = document.querySelector("#no-repeat");
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  // Create an object with the numbers details
+  const newDraw = {
+    quantity: Number(inputNumber.value),
+    from: Number(fromNumber.value),
+    to: Number(toNumber.value),
+  };
+
+  drawNumbers(newDraw.quantity, newDraw.from, newDraw.to);
+
+  console.log(drawNumbers(newDraw.quantity, newDraw.from, newDraw.to));
+};
+
+function drawNumbers(quantity, from, to) {
+  let randomNubers = [];
+  let getRandomNumber = () =>
+    Math.floor(Math.random() * (to - from + 1)) + from;
+
+  // If input is checked converts in a array of unique numbers
+  if (uniqueNumber.checked) {
+    randomNubers = [...new Set(randomNubers)];
+  }
+
+  // Add a new random numbers until the array length is equal to the quntity
+  while (randomNubers.length < quantity) {
+    randomNubers.push(getRandomNumber());
+  }
+
+  return randomNubers;
+}
