e3735e1e70ba055edac44ec15a67229771dd3676
Juliana Braga <jujbraga@gmail.com>
2025-09-26 11:49:26 +0200

    add script

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Select form elements
+const form = document.querySelector("form");
+const inputNumber = document.getElementById("numbers");
+const fromNumber = document.getElementById("from");
+const toNumber = document.getElementById("to");
+const uniqueNumber = document.querySelector("#no-repeat");
