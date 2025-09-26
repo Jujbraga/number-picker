72281cfcb59d62aee85c7ff80bbcffbc15a41bca
Juliana Braga <jujbraga@gmail.com>
2025-09-26 21:22:06 +0200

    add events to form (input, focusout, submit)

--- scripts.js
+++ scripts.js
@@ -5,20 +5,41 @@ const fromNumber = document.getElementById("from");
 const toNumber = document.getElementById("to");
 const uniqueNumber = document.querySelector("#no-repeat");
 
-form.onsubmit = (event) => {
-  event.preventDefault();
+function setFormEvents() {
+  // Format input to accept just numbers
+  form.addEventListener("input", (event) => {
+    if (event.target.matches('form input[type="text"]')) {
+      let value = event.target.value.replace(/\D/g, "");
+      event.target.value = value;
+    }
+  });
 
-  // Create an object with the numbers details
-  const newDraw = {
-    quantity: Number(inputNumber.value),
-    from: Number(fromNumber.value),
-    to: Number(toNumber.value),
+  // Set the input value to 1 if it is empty
+  form.addEventListener("focusout", (event) => {
+    if (event.target.matches('form input[type="text"]')) {
+      if (event.target.value === "" || event.target.value === "0") {
+        event.target.value = "1";
+      }
+    }
+  });
+
+  // Onsubmit prevent reload and create an object with the numbers details
+  form.onsubmit = (event) => {
+    event.preventDefault();
+
+    const newDraw = {
+      quantity: Number(inputNumber.value),
+      from: Number(fromNumber.value),
+      to: Number(toNumber.value),
+    };
+
+    drawNumbers(newDraw.quantity, newDraw.from, newDraw.to);
   };
+}
 
-  drawNumbers(newDraw.quantity, newDraw.from, newDraw.to);
+setFormEvents();
 
-  console.log(drawNumbers(newDraw.quantity, newDraw.from, newDraw.to));
-};
+function onlyPositiveNumber() {}
 
 function drawNumbers(quantity, from, to) {
   let randomNubers = [];
@@ -30,10 +51,12 @@ function drawNumbers(quantity, from, to) {
     randomNubers = [...new Set(randomNubers)];
   }
 
-  // Add a new random numbers until the array length is equal to the quntity
+  // Add a new random numbers until the array length is equal to the quantity
   while (randomNubers.length < quantity) {
     randomNubers.push(getRandomNumber());
   }
 
   return randomNubers;
 }
+
+// Show the draw result
